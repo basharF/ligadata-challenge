b297344bfec87ed393b810d2886dc9abc9513fa5
basharF <bashar.fares.sy@gmail.com>
2019-09-03 13:07:50 +0300

    secondCommit

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,45 +9,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  inputsFocused: boolean[] = [false, false, false, false, false];
   user = new User();
   confirmPassword: string;
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-
   }
 
   signup() {
     this.userService.signup(this.user);
   }
-
-  focusFunction(i: HTMLElement) {
-    switch (i.id) {
-      case 'i1':
-          this.inputsFocused[0] = true;
-          break;
-      case 'i2':
-          this.inputsFocused[1] = true;
-          break;
-      case 'i3':
-          this.inputsFocused[2] = true;
-          break;
-    }
-  }
-
-  focusOutFunction(i) {
-    switch (i.id) {
-      case 'i1':
-          this.inputsFocused[0] = false;
-          break;
-      case 'i2':
-          this.inputsFocused[1] = false;
-          break;
-      case 'i3':
-          this.inputsFocused[2] = false;
-          break;
-    }
-  }
-
 }
